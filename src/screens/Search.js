2d79c4beda3687bc891b54524f8e4ade14fd563a
Douglas Maringa <douglasmaringa4@gmail.com>
2021-06-17 18:47:25 +0200

    Update Search.js

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -12,14 +12,11 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import Checkbox from '@material-ui/core/Checkbox';
 import {db} from "../firebase"
-import mapboxgl from '!mapbox-gl';
 import ReactMapGL, { Marker, Popup } from "react-map-gl";
-import mapboxgl from "mapbox-gl"
 import walk from "../Data/Walk.gif"
 import { useStateValue } from "../StateProvider";
 import {useHistory} from 'react-router-dom'
-//eslint-disable-next-line import/no-webpack-loader-syntax
-mapboxgl.workerClass = require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
+
 
 
 
